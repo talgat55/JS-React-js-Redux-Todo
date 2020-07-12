30470e305700f7f6caa76d87092271a9c24e52fc
talgat55 <lightx_design@yahoo.com>
2020-07-12 15:31:02 +0600

    end

--- src/components/VisibilityFilter.jsx
+++ src/components/VisibilityFilter.jsx
@@ -1,9 +1,29 @@
 import  React from 'react';
-
-const VisibilityFilter = ({filter}) =>{
+import {connect} from 'react-redux';
+import {setFilter} from "../store/actions";
+import {Filters} from "../store/types";
+const VisibilityFilter = ({activeFilter, setFilter}) =>{
     return(
-        filter.map((item,index) => <button key={`filter-${index}`}>{item}</button> )
+        Filters.map((filter,index) => (
+            <button
+                className={filter === activeFilter ? 'active' : ''}
+                onClick={() => setFilter(filter)}
+                key={`filter-${index}`}
+
+            >
+                {filter}
+            </button>
+        ))
     )
 };
-export default  VisibilityFilter;
+
+const mapStateToProps = state =>{
+    return {
+        activeFilter: state.visibilityFilter.activeFilter
+    }
+};
+const mapDispatchToProps = {
+    setFilter
+};
+export default connect(mapStateToProps, mapDispatchToProps)(VisibilityFilter);
 
